0af9833c40dd94a1739c67ee5d3dbb797b43b1a4
lsh <jetaime12121@gmail.com>
2021-12-09 09:51:08 +0900

    vue swiper 적용

---
+++ front/src/components/common/Nav.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="wrapNav">
+        <h2>HOME</h2>
+        <hr>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    .wrapNav {height: 200px;}
+    h2 {margin-bottom: 30px;}
+</style>
---
+++ front/src/views/MainView.vue
@@ -0,0 +1,65 @@
+
+<template>
+  <swiper class="swiper" :options="swiperOption">
+    <swiper-slide>Slide 1</swiper-slide>
+    <swiper-slide>Slide 2</swiper-slide>
+    <swiper-slide>Slide 3</swiper-slide>
+    <swiper-slide>Slide 4</swiper-slide>
+    <swiper-slide>Slide 5</swiper-slide>
+    <swiper-slide>Slide 6</swiper-slide>
+    <swiper-slide>Slide 7</swiper-slide>
+    <swiper-slide>Slide 8</swiper-slide>
+    <swiper-slide>Slide 9</swiper-slide>
+  </swiper>
+</template>
+
+<script>
+  import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
+  import 'swiper/css/swiper.css'
+
+  export default {
+    name: 'swiper-example-loop-group',
+    title: 'Loop mode with multiple slides per group',
+    components: {
+      Swiper,
+      SwiperSlide
+    },
+    data() {
+      return {
+        swiperOption: {
+          slidesPerView: 2.3,
+          spaceBetween: 150,
+          loop: true,
+          loopFillGroupWithBlank: true,
+          pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+          },
+          navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+          }
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .swiper {
+  height: 500px;
+  width: 100%;
+
+  .swiper-slide {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    font-weight: bold;
+    background: #000;
+    color: #fff;
+  }
+
+}
+
+</style>
